b4fc47a73fbfe987827c0462e162bf8bf7d33e82
MiltonLiquinchana <liquinchana@hotmail.es>
2020-03-31 06:33:53 -0500

    En este primer intento se logro imprimir el hola mundo

---
+++ src/main/java/ec/edu/intsuperior/PrimerSprigProjectApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.intsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerSprigProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerSprigProjectApplication.class, args);
+	}
+
+}
